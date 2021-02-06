644db29c5dfe4f758e1c4b4070f507b708efe413
Ihor Husar <ihor.husar@gmail.com>
2021-02-06 14:10:01 -0500

    Added a compose-file with initializing script. Restart of the server is intentional

---
+++ docker-entrypoint-initdb.d/initDB.sql
@@ -0,0 +1,23 @@
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+CREATE EXTENSION postgis;
+
+\c spacex
+
+
+CREATE TABLE satelites (
+	id SERIAL PRIMARY KEY,
+	name TEXT
+);
+
+CREATE TABLE satelite_positions (
+	recorded_at TIMESTAMP,
+	satelite_id INT,
+	longitude NUMERIC,
+	latitude NUMERIC,
+	CONSTRAINT fk_satelite
+		FOREIGN KEY(satelite_id)
+			REFERENCES satelites(id)
+);
+
+SELECT create_hypertable("satelite_positions", "recorded_at", "satelite_id", 3, create_default_indexes=>FALSE);
+CREATE INDEX ON satelite_positions (satelite_id, recorded_at DESC);
